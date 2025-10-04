0ab809a607c55a9e868356bd236446853217eeb3
Jade Turner <spacey-sooty@proton.me>
2025-10-04 17:25:40 +0800

    More fixes
    Signed-off-by: Jade Turner <spacey-sooty@proton.me>

--- install.sh
+++ install.sh
@@ -1,6 +1,7 @@
 mkdir -p /opt/outreach/images
 cp images/* /opt/outreach/images
-sudo cp outreach_disply.service /etc/systemd/system/
+sudo mkdir -p /etc/systemd/system/
+sudo cp outreach_disply.service /etc/systemd/system/outreach_display.service
 cp display_images.sh /opt/outreach/display_images.sh
 sudo apt update
 sudo apt install fbi
